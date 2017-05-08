474e949528e5303417ee510faeb09c8e245896ff
Viviana Salazar <Viviana Salazar>
2017-05-08 03:34:35 -0400

    Preparinf files for Heroku dep.

--- web_events.py
+++ web_events.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import facebook_run
-#import os
+import os
 
 app = Flask(__name__)
 
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/results")  # crea otra pagina. Esta es el About
-def results():   #name of the function to call the page about in html
+@app.route("/results")  
+def results():   
     city = request.values.get('city')
     num_results = request.values.get('num_results')
     if city and num_results:
@@ -18,15 +18,15 @@ def results():   #name of the function to call the page about in html
         city = "New York"
         num_results = 5
         events_list = facebook_run.search_events(city, num_results)
-    return render_template('results.html', city = city, num_results = num_results, events_list= events_list )	#calls the html file
+    return render_template('results.html', city = city, num_results = num_results, events_list= events_list )	
 
-@app.route("/about")  # crea otra pagina. Esta es el About
-def about():   #name of the function to call the page about in html
-    return render_template('about.html')  #calls the html file	
+@app.route("/about")  
+def about():  
+    return render_template('about.html')  	
 
 if __name__ == "__main__":
-    app.run()  #when wokring on a local port
-    #port = int(os.environ.get("PORT", 5000))  #when deploying it to Heroku
-    #app.run(host="0.0.0.0", port=port)
+    #app.run()  #when wokring on a local port
+    port = int(os.environ.get("PORT", 5000))  #when deploying it to Heroku
+    app.run(host="0.0.0.0", port=port)
 
 
